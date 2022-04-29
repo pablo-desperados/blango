51f307c2873162612c43a272467e9be69c544e5c
Pablo Mujica <pblmjc@gmail.com>
2022-04-29 15:06:50 +0000

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as bl
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",bl.index)
 ]
